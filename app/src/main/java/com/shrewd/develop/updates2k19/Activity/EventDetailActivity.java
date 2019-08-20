d9538bf85e2096b3164d1fcdc3ced76fbb75bdff
GHELANI <nevilghelani99612@gmail.com>
2019-08-20 21:22:45 +0530

    Commit at 9:22 20/8/2019 by Nevil

---
+++ app/src/main/java/com/shrewd/develop/updates2k19/Activity/EventDetailActivity.java
@@ -0,0 +1,15 @@
+package com.shrewd.develop.updates2k19.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shrewd.develop.updates2k19.R;
+
+public class EventDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events_detail);
+    }
+}
